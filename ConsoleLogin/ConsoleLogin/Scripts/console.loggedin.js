675cb003a71a7fda82aab74646658ba6f20ef6de
jaypeagi <git@average-joe.info>
2014-02-06 12:39:32 +0000

    Basic MVC and a proof of concept (2)

---
+++ ConsoleLogin/ConsoleLogin/Scripts/console.loggedin.js
@@ -0,0 +1,36 @@
+﻿window.console = {
+    _console: window.console,
+    log: function (msg) {
+
+        //TODO: Send to server
+        this._console.log("TEST: " + msg + " from " + window.location);
+    },
+    logto: function (usr, msg) {
+        //TODO: DM
+        return "User offline.";
+    },
+    login: function () {
+        //called automatically when first logged in, also if session expires.
+
+        this._console.group();
+
+        //TODO: OAuth
+
+        this._console.log("Logged in. Type console.help() for help.");
+
+        //TODO: Listen for updates
+    },
+    logout: function () {
+        this._console.log("Logging out...");
+
+        window.console = this._console;
+
+        this._console.groupEnd();
+
+        return "Logged out. Normality Restored.";
+    },
+    help: function () {
+        return "Console.Login is a secret social network. It can be implemented into any site simply by including a single JavaScript file. Talk to others logged into Console.Login by using the console.log(...) command.";
+    }
+};
+console.login();
---
+++ ConsoleLogin/ConsoleLogin/Scripts/console.login.js
@@ -0,0 +1,12 @@
+﻿if (!console) console = { log: function () { } };
+
+console.login = function () {
+    console.log("Logging in...");
+    var fileref = document.createElement('script');
+    fileref.setAttribute("type", "text/javascript");
+    fileref.setAttribute("src", "/Scripts/console.loggedin.js");
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+    return "...";
+};
+
+console.log("Join in by typing console.login().");
