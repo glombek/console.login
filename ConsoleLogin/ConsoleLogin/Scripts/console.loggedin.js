1a06212133cff6c635b301b0a37ee49544c12e88
jaypeagi <git@average-joe.info>
2014-02-06 12:52:10 +0000

    neatenning up appearence of POC

--- ConsoleLogin/ConsoleLogin/Scripts/console.loggedin.js
+++ ConsoleLogin/ConsoleLogin/Scripts/console.loggedin.js
@@ -1,4 +1,4 @@
-﻿window.console = {
+﻿window.console.consolelogin = {
     _console: window.console,
     log: function (msg) {
 
@@ -11,12 +11,11 @@
     },
     login: function () {
         //called automatically when first logged in, also if session expires.
-
-        this._console.group();
+        window.console = this;
 
         //TODO: OAuth
 
-        this._console.log("Logged in. Type console.help() for help.");
+        return "Logged in. Type console.help() for help.";
 
         //TODO: Listen for updates
     },
@@ -33,4 +32,4 @@
         return "Console.Login is a secret social network. It can be implemented into any site simply by including a single JavaScript file. Talk to others logged into Console.Login by using the console.log(...) command.";
     }
 };
-console.login();
+console.log(window.console.consolelogin.login());
--- ConsoleLogin/ConsoleLogin/Scripts/console.login.js
+++ ConsoleLogin/ConsoleLogin/Scripts/console.login.js
@@ -1,12 +1,20 @@
-﻿if (!console) console = { log: function () { } };
+﻿if (!!console) {
 
-console.login = function () {
-    console.log("Logging in...");
-    var fileref = document.createElement('script');
-    fileref.setAttribute("type", "text/javascript");
-    fileref.setAttribute("src", "/Scripts/console.loggedin.js");
-    document.getElementsByTagName("head")[0].appendChild(fileref);
-    return "...";
-};
+    console.login = function () {
+        console.group();
+        if (!window.console.consolelogin) {
+            console.log("Logging in...");
+            var fileref = document.createElement('script');
+            fileref.setAttribute("type", "text/javascript");
+            fileref.setAttribute("src", "/Scripts/console.loggedin.js");
+            document.getElementsByTagName("head")[0].appendChild(fileref);
+            return "...";
+        } else {
+            console.log("Already set up, logging in...");
+            return window.console.consolelogin.login();
+        }
+    };
 
-console.log("Join in by typing console.login().");
+    console.log("Join in by typing console.login().");
+
+}
